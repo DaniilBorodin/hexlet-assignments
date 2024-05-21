68b5af3d3056f42606824662dbd730b48926d170
ruabidd <Daniil.BORODIN@raiffeisen.ru>
2024-05-21 14:12:50 +0300

    fix annotations

--- java-spring-ru/first-start/src/test/java/exercise/ApplicationTest.java
+++ java-spring-ru/first-start/src/test/java/exercise/ApplicationTest.java
@@ -1,8 +1,13 @@
 package exercise;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootApplication
+@RestController
 class ApplicationTest {
 
     @Test
